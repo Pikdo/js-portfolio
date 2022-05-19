be1f639718736f233eeb3e39d52e2459356e1819
Pikdo <ali87to@gmail.com>
2022-05-19 14:53:04 -0600

    configuración netlify

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import '@styles/vars.styl';
 
 console.log("Webpack es muy útil");
 console.log("Prepara el proyecto para producción");
+console.log("Optimiza el código, lo minimiza y los ofusca");
 
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
-
 })();
