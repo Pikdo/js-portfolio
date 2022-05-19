9e322ec1b5667cb5c5230d75a9cbe6ded5081f34
Pikdo <ali87to@gmail.com>
2022-05-19 08:38:30 -0600

    Repo conectado a github

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
 
-var confetti = require('canvas-confetti');
-
-confetti();
-
 console.log("Webpack es muy útil");
 console.log("Prepara el proyecto para producción");
 
